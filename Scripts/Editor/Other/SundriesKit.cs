d44a275bfbc59cdb1ad19d3d078017e86fa276cf
cofdream <cofdream@sina.com>
2022-11-11 19:45:07 +0800

    add unity刷新并播放

--- Scripts/Editor/Other/SundriesKit.cs
+++ Scripts/Editor/Other/SundriesKit.cs
@@ -13,7 +13,7 @@ namespace Cofdream.ToolKitEditor
     {
         private const string HEAD = MenuItemUtil.ToolKit + "杂物/";
 
-        #region 触发编译并且播放
+        #region XXXX后播放
         private const string IS_PLAY_EDITOR_AFTER_COMPILATION = "ISPlayEditorAfterCompilation";
 
         [MenuItem(HEAD + "触发脚本编译,结束后自动播放")]
@@ -31,7 +31,15 @@ namespace Cofdream.ToolKitEditor
                 EditorPrefs.DeleteKey(IS_PLAY_EDITOR_AFTER_COMPILATION);
                 EditorApplication.EnterPlaymode();
             }
-        } 
+        }
         #endregion
+
+        [MenuItem(HEAD + "刷新资源并播放")]
+        private static void RefreshAndPlay()
+        {
+            AssetDatabase.Refresh();
+            EditorApplication.EnterPlaymode();
+        }
+
     }
 }
