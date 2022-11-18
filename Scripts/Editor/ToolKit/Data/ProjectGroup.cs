1bdc0d9cd2019dac712842c27edf2e7a29646f52
cofdream <cofdream@sina.com>
2022-11-18 09:42:03 +0800

    数据结构调整

---
+++ Scripts/Editor/ToolKit/Data/ProjectGroup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Cofdream.ToolKitEditor
+{
+    [System.Serializable]
+    public class ProjectGroup : ScriptableObject
+    {
+        public string Name;
+
+        public ProjectInfo[] ProjectInfos;
+
+        private void OnValidate()
+        {
+            // todo 把 ProjectInfos 改造为 sub asset
+        }
+    }
+}
