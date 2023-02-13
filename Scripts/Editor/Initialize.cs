a20ca53b0dc7783ad4c8de7e902c7d6231d0c4f6
cofdream <cofdream@sina.com>
2023-02-14 00:26:29 +0800

    新增延迟调用接口和相关属性，防止反射调用内部函数时机过早

--- Scripts/Editor/Initialize.cs
+++ Scripts/Editor/Initialize.cs
@@ -5,17 +5,24 @@ using UnityEditor;
 using System.Reflection;
 using System;
 using UnityEngine.Assertions.Must;
+using System.Linq;
+using UnityEditor.Compilation;
+using NUnit.Framework.Internal;
 
 namespace Cofdream.ToolKitEditor
 {
-    [AttributeUsage(AttributeTargets.Class)]
-    public class InitializeOnLoadAttribute : Attribute { }
+    //[AttributeUsage(AttributeTargets.Class)]
+    //public class InitializeOnLoadAttribute : Attribute { }//UnityEditor.InitializeOnLoadAttribute { }
+
+    //[AttributeUsage(AttributeTargets.Method)]
+    //public class InitializeOnLoadMethodAttribute : Attribute { }//UnityEditor.InitializeOnLoadMethodAttribute { }
+
+    //[AttributeUsage(AttributeTargets.Method)]
+    //public class InitializeOnEnterPlayModeAttribute : Attribute { }//UnityEditor.InitializeOnEnterPlayModeAttribute { }
+
 
     [AttributeUsage(AttributeTargets.Method)]
-    public class InitializeOnLoadMethodAttribute : Attribute { }
-
-    [AttributeUsage(AttributeTargets.Method)]
-    public class InitializeOnEnterPlayModeAttribute : Attribute { }
+    public class DelayCallAttribute : Attribute { }
 
 
     [UnityEditor.InitializeOnLoad]
@@ -23,51 +30,110 @@ namespace Cofdream.ToolKitEditor
     {
         static Initialize()
         {
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                foreach (var type in assembly.GetTypes())
-                {
-                    if (type.GetCustomAttribute(typeof(InitializeOnLoadAttribute), false) != null)
-                    {
-                        System.Runtime.CompilerServices.RuntimeHelpers.RunClassConstructor(type.TypeHandle);
-                    }
-                }
-            }
+            //foreach (var type in TypeCache.GetTypesWithAttribute<InitializeOnLoadAttribute>())
+            //{
+            //    if (type.GetCustomAttribute(typeof(InitializeOnLoadAttribute), false) != null)
+            //    {
+            //        System.Runtime.CompilerServices.RuntimeHelpers.RunClassConstructor(type.TypeHandle);
+            //    }
+            //}
+
+
+            //foreach (var method in TypeCache.GetMethodsWithAttribute<InitializeOnLoadMethodAttribute>())
+            //{
+            //    if (method.ContainsGenericParameters)
+            //    {
+            //        Debug.LogError($"不能对泛型函数添加: {nameof(InitializeOnLoadMethodAttribute)} \n{method.Name}");
+            //        continue;
+            //    }
+
+            //    if (method.GetParameters().Length > 0)
+            //    {
+            //        Debug.LogError($"不能对带有参数的函数添加: {nameof(InitializeOnLoadMethodAttribute)} \n{method.Name}");
+            //        continue;
+            //    }
+            //    if (method.IsStatic == false)
+            //    {
+            //        Debug.LogError($"不能对非静态函数添加: {nameof(InitializeOnLoadMethodAttribute)} \n{method.Name}");
+            //        continue;
+            //    }
+            //    try
+            //    {
+            //        method.Invoke(null, null);
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Debug.LogError($"调用 {nameof(InitializeOnLoadMethodAttribute)} \n{method.Name} Exception:\n{e}");
+            //    }
+            //}
+
+            EditorApplication.delayCall += DelayCall;
         }
 
-        [UnityEditor.InitializeOnLoadMethod]
-        private static void InitializeOnLoad()
+        private static void DelayCall()
         {
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            EditorApplication.delayCall -= DelayCall;
+
+            foreach (var method in TypeCache.GetMethodsWithAttribute<DelayCallAttribute>())
             {
-                foreach (var type in assembly.GetTypes())
+                if (method.ContainsGenericParameters)
                 {
-                    foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.InvokeMethod))
-                    {
+                    Debug.LogError($"不能对泛型函数添加: {nameof(DelayCallAttribute)} \n{method.Name}");
+                    continue;
+                }
 
-                        if (method.GetCustomAttribute(typeof(InitializeOnLoadMethodAttribute), false) != null)
-                        {
-                            if (method.GetParameters().Length == 0)
-                            {
-
-                                method.Invoke(null, BindingFlags.InvokeMethod, Type.DefaultBinder, new object[0], null);
-                            }
-                            else
-                            {
-                                // todo 可变参数 剔除
-                                try
-                                {
-                                    method.Invoke(null, BindingFlags.InvokeMethod, Type.DefaultBinder, new object[0], null);
-                                }
-                                catch (Exception e)
-                                {
-                                    Debug.LogError(e);
-                                }
-                            }
-                        }
-                    }
+                if (method.GetParameters().Length > 0)
+                {
+                    Debug.LogError($"不能对带有参数的函数添加: {nameof(DelayCallAttribute)} \n{method.Name}");
+                    continue;
+                }
+                if (method.IsStatic == false)
+                {
+                    Debug.LogError($"不能对非静态函数添加: {nameof(DelayCallAttribute)} \n{method.Name}");
+                    continue;
+                }
+                try
+                {
+                    method.Invoke(null, null);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"调用 {nameof(DelayCallAttribute)} \n{method.Name} Exception:\n{e}");
                 }
             }
         }
+   
+
+        //[UnityEditor.InitializeOnLoadMethod]
+        //private static void InitializeOnLoad()
+        //{
+        //    foreach (var method in TypeCache.GetMethodsWithAttribute<InitializeOnLoadMethodAttribute>())
+        //    {
+        //        if (method.ContainsGenericParameters)
+        //        {
+        //            Debug.LogError($"不能对泛型函数添加: {nameof(InitializeOnLoadMethodAttribute)} \n{method.Name}");
+        //            continue;
+        //        }
+
+        //        if (method.GetParameters().Length > 0)
+        //        {
+        //            Debug.LogError($"不能对带有参数的函数添加: {nameof(InitializeOnLoadMethodAttribute)} \n{method.Name}");
+        //            continue;
+        //        }
+        //        if (method.IsStatic == false)
+        //        {
+        //            Debug.LogError($"不能对非静态函数添加: {nameof(InitializeOnLoadMethodAttribute)} \n{method.Name}");
+        //            continue;
+        //        }
+        //        try
+        //        {
+        //            method.Invoke(null, null);
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            Debug.LogError($"调用 {nameof(InitializeOnLoadMethodAttribute)} \n{method.Name} Exception:\n{e}");
+        //        }
+        //    }
+        //}
     }
 }
